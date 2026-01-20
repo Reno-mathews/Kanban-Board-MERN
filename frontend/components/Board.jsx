dda16a5556f1364b62d28f580915c18ad0f49f64
Reno-mathews <renomathews10pro@gmail.com>
2026-01-20 14:08:12 +0530

    Added SearchBar

---
+++ frontend/components/Board.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Column from "./Column";
+
+function Board() {
+    const [columns, setColumns] = useState([
+    {
+        id: 1,
+        title: "To Do",
+        tasks: [
+            { id: 1, title: "Study React"},
+            { id: 2, title: "Study Express"},
+        ],
+    },
+    {
+        
+    }
+    ])
+}
