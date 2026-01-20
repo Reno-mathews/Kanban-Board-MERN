bfc23165792835979a053f59bd743e0a2e90bcff
Reno-mathews <renomathews10pro@gmail.com>
2026-01-20 08:37:28 +0530

    Made Kanban board

---
+++ frontend/components/AuthForm.jsx
@@ -0,0 +1,18 @@
+function AuthForm ({
+    isSignup,
+    setIsSignUp,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    onLogin,
+    onSignup,
+}) {
+    return (
+        <div className="">
+            <div className="">
+                <h2 className="text-2xl font-bold"
+            </div>
+        </div>
+    )
+}
