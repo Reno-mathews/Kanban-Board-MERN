1323dc370989c8c7ba140fa91940565cfe0d4952
Reno-mathews <renomathews10pro@gmail.com>
2026-01-20 18:58:44 +0530

    Added Board.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
-
+import Board from "./components/Board";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+      <div className="nin-h-screen bg-gray-900 text-white">
+        <Board />
+      </div>
+  );
+}
 
-
-export default App
+export default App;
