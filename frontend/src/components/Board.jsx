2901a7678d4f2547a31afd158d6325ab1ccc79bd
Reno-mathews <renomathews10pro@gmail.com>
2026-01-21 12:35:33 +0530

    Added Modal

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -48,7 +48,38 @@ function Board() {
                 + Add Task
             </button>
 
-        {/* Display columns */}
+            {/* Modal */}
+
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                    <div className="bg-gray-800 p-6 rounded-lg w-80">
+                        <h2 className="text-lg font-semibold mb-4">Add New Task</h2>
+
+                        <input
+                            type="text"
+                            placeholder="Task title"
+                            clasName="w-full p-2 rounded bg-gray-700 mb-4"
+                        />
+
+                        <div className="flex justify-end gap-2">
+                            <button 
+                                onClick={() => setIsModalOpen(false)}
+                                className="px-3 py-1 bg-gray-600 rounded"
+                            >
+                                Cancel
+                            </button>
+
+                            <button 
+                                className="px-3 py-1 bg-blue-600 rounded"
+                            >
+                                Add
+                            </button>
+                        </div>
+                </div>
+            </div>
+            )}
+
+            {/* Display columns */}
             <div className="flex gap-6 p-6 overflow-x-auto">
                 {columns.map((column) => {
                     const filteredTasks = column.tasks.filter((task) =>
