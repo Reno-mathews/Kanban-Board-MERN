b0945689becefbf5d944e1d89eac74d40bb53747
Reno-mathews <renomathews10pro@gmail.com>
2026-01-22 16:06:43 +0530

    Wrapped rach task in a dragabble wrapper

---
+++ .history/frontend/src/components/Column_20260122155823.jsx
@@ -0,0 +1,20 @@
+import { SortableContext, verticalListSorting} from "@dnd-kit/sortable";
+import TaskCard from "./TaskCard";
+
+function Column({ column }) {
+    return(
+        <div className="bg-gray-800 rounded-lg p-4 w-72 flex-shrink-0">
+            <h2 className="text-lg font-semibold mb-4 text-center">
+                {column.title}
+            </h2>
+
+            <div className="space-y-3">
+                {column.tasks.map((task) => (
+                    <TaskCard key={task.id} task={task} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Column;
