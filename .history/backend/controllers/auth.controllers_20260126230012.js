fad5d8f0e7dda887a2c453d4883607a3b901e4d5
Reno-mathews <renomathews10pro@gmail.com>
2026-01-28 12:33:45 +0530

    Practicing

---
+++ .history/backend/controllers/auth.controllers_20260126230012.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (user) => {
+    return jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+    );
+};
+
+// SIGNUP
+exports.signup = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password)
+        return res.status(400).json({ message: "Missing fields" });
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+        return res.status(400).json({ message: "User already exists"});
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        email,
+        passwordHash,
+    });
+
+    const token = generateToken(user);
+
+    res.status(201).json({
+        token,
+        user: {
+            id: user._id,
+            email: user.email,
+            role: user.role,
+        },
+    });
+};
+
+// LOGIN 
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if(!user)
+        return res.status(400).json({ message: "Invalid credentials "});
+
+    const isMatch = await bcrypt.compare(password, user.passwordHash);
+
+    if(!isMatch)
+        return res.status(400).json({ message: "Invalid credentials "});
+
+    const token = generateToken(user);
+
+    res.json({
+        token,
+        user: {
+            id: user._id,
+            email: user.email,
+            role: user.role,
+        },
+    });
+};
