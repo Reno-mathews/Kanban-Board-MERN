54d83d8a7daee6a829d9d3b29e25f85cee5a6129
Reno-mathews <renomathews10pro@gmail.com>
2026-01-29 10:07:23 +0530

    Fixing the frontend ui

--- .history/frontend/src/components/AuthForm_20260129100122.jsx
+++ .history/frontend/src/components/AuthForm_20260129100122.jsx
@@ -24,16 +24,14 @@ function AuthForm ({
                         }}
                         className="space-y-4"
                     >
-                        <div className="space-y-1">
-                            <label className="text-sm text-gray-300">Email</label>
                             <input
-                            type="email"
-                             className="w-full px-4 py-2 rounded-md bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                             placeholder="you@example.com"
+                             className=""
+                             type="email"
+                             placeholder="Email"
                              value={email}
                              onChange={(e) => setEmail(e.target.value)}
                             />
-                        </div>
+
                             <input 
                                 className=""
                                 type="password"
