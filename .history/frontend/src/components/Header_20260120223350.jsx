cffe028581ceccef2dc35e819335d8837bcc0947
Reno-mathews <renomathews10pro@gmail.com>
2026-01-29 18:08:46 +0530

    Made some fixes

--- .history/frontend/src/components/Header_20260120223350.jsx
+++ .history/frontend/src/components/Header_20260120223350.jsx
@@ -1,16 +1,9 @@
-function Header({ onLogout }) {
+function Header() {
     return (
         <div className="flex items-center justify-between px-6 py-4 bg-gray-800 border-b border-gray-700">
         <h1 className="text-3xl font-bold text-center">
             Kanban Board
         </h1>
-
-        <button
-            onClick={onLogout}
-            className="text-sm text-gray-400 hover:text-white transition"
-        >
-            Logout
-        </button>
         <p className="text-sm text-gray-400">My Study Board</p>
         </div>
     );
